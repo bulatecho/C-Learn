f04911d7fb50e4f37979981c081a85166a113e01
bulatecho <90001879+bulatecho@users.noreply.github.com>
2023-03-04 09:44:13 +0500

    4march

--- where.cs
+++ where.cs
@@ -1,1 +1,11 @@
+class WhereSample
+{
+     static void Main()
+     int[] numbers = {5, 4, 1, 3, 9, 8, 1};
 
+     //Simple query with one predicate in where clause
+     var queryLowNums = 
+        from num in numbers
+        where num < 5 
+        select num;
+}
